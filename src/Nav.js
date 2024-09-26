f1ebe4e30b0b7db974b91adba043741835427ee8
Danny <dannypascazio@gmail.com>
2024-09-26 17:47:40 +0200

    Implementing first navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,29 @@
 const Nav = () => {
   return (
     <>
-      <nav></nav>
+      <nav>
+        <img src="/Logo.svg"></img>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/menu">Menu</a>
+          </li>
+          <li>
+            <a href="/reservations">Reservations</a>
+          </li>
+          <li>
+            <a href="/order-online">Order Online</a>
+          </li>
+          <li>
+            <a href="/login">Login</a>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 };
