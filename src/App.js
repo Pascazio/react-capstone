1d50553c919e72ddbc6bc96e51d016e646e5be16
Danny <dannypascazio@gmail.com>
2024-09-26 14:21:35 +0200

    Setting up the structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
-
+import "./App.css";
+import Nav from "./Nav";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Nav />
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
