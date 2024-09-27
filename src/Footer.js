353007782f337e666b2bac77df9c94eab0c7d41a
Danny <dannypascazio@gmail.com>
2024-09-27 21:09:30 +0200

    Adding semantic html to footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,54 @@
 const Footer = () => {
   return (
     <>
-      <footer></footer>
+      <footer>
+        <img src="/Logo.svg"></img>
+        <ul>
+          <li>Doormat navigation</li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/menu">Menu</a>
+          </li>
+          <li>
+            <a href="/reservations">Reservations</a>
+          </li>
+          <li>
+            <a href="/order-online">Order Online</a>
+          </li>
+          <li>
+            <a href="/login">Login</a>
+          </li>
+        </ul>
+        <ul>
+          <li>Contact</li>
+          <li>
+            <a href="/contact">Address</a>
+          </li>
+          <li>
+            <a href="/contact">Phone number</a>
+          </li>
+          <li>
+            <a href="/contact">Email</a>
+          </li>
+        </ul>
+        <ul>
+          <li>Social media links</li>
+          <li>
+            <a href="www.twitter.com">X</a>
+          </li>
+          <li>
+            <a href="www.instagram.com">Instagram</a>
+          </li>
+          <li>
+            <a href="www.facebook.com">Facebook</a>
+          </li>
+        </ul>
+      </footer>
     </>
   );
 };
