418eff4e1208c59acb7d899de4d28898e374cfdb
Danny <dannypascazio@gmail.com>
2024-10-08 15:37:50 +0200

    Header skeleton up

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,16 @@
 const Header = () => {
   return (
     <>
-      <header></header>
+      <header>
+        <div className="cta">
+          <h1>Little Lemon Restaurant</h1>
+          <p>Where the Mediterranean meets Chicago</p>
+          <button className="reserve-button">Reserve a table</button>
+        </div>
+        <div className="hero">
+          <img src="restaurant.jpg" alt="Restaurant" />
+        </div>
+      </header>
     </>
   );
 };
