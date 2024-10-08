782bc06bd4e4e5898bf84086e7044a08827e187e
Danny <dannypascazio@gmail.com>
2024-10-08 15:44:06 +0200

    Adding link to reservations

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,9 @@ const Header = () => {
         <div className="cta">
           <h1>Little Lemon Restaurant</h1>
           <p>Where the Mediterranean meets Chicago</p>
-          <button className="reserve-button">Reserve a table</button>
+          <button className="reserve-button">
+            <a href="/reservations">Reserve a table</a>
+          </button>
         </div>
         <div className="hero">
           <img src="restaurant.jpg" alt="Restaurant" />
