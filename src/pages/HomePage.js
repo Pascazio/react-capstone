4b7f3ce432abfbbc5de0410ca63cda7ba16d2818
Danny <dannypascazio@gmail.com>
2024-10-10 20:33:14 +0200

    Homepage setup

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,12 @@
-import Header from "../components/Header";
-import Main from "../components/Main";
-const HomePage = () => {
-    return (
-        <>
-        <Header></Header>
-        <Main></Main>
-        </>
-    )
-}
+    import Header from "../components/Header";
+    import Main from "../components/Main";
+    const HomePage = () => {
+        return (
+            <>
+            <Header></Header>
+            <Main></Main>
+            </>
+        )
+    }
 
-export default HomePage;
+    export default HomePage;
