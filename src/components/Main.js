3b7ab7e6aaa0ee3b28e9eccd946745b4fece3775
Danny <dannypascazio@gmail.com>
2024-10-14 13:01:13 +0200

    Adding initializeTimes api call

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,11 @@
-import React, { useReducer } from 'react';
-import BookingForm from './BookingForm';
+import React, { useReducer } from "react";
+import BookingForm from "./BookingForm";
+import { fetchAPI as fetchData } from "../js/Api";
 
 export const initializeTimes = () => {
-  return ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+  const today = new Date();
+  const response = fetchData(today);
+  return response;
 };
 
 export const updateTimes = (state, action) => {
@@ -12,9 +15,7 @@ export const updateTimes = (state, action) => {
 const Main = () => {
   const [availableTimes, dispatch] = useReducer(updateTimes, [], initializeTimes);
 
-  return (
-    <BookingForm availableTimes={availableTimes} dispatch={dispatch} />
-  );
+  return <BookingForm availableTimes={availableTimes} dispatch={dispatch} />;
 };
 
 export default Main;
